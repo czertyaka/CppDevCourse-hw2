81b910a11e82d228afcce9b23669ad72ccb96b57
Jopick <piple05@mail.ru>
2024-11-27 18:25:40 +0500

    added main.cpp

---
+++ main.cpp/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivan/Desktop/CppDevCourse-hw2
+# Build directory: /home/ivan/Desktop/CppDevCourse-hw2/main.cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(main "/usr/bin/python3" "-m" "test" "./main")
+set_tests_properties(main PROPERTIES  WORKING_DIRECTORY "/home/ivan/Desktop/CppDevCourse-hw2/main.cpp" _BACKTRACE_TRIPLES "/home/ivan/Desktop/CppDevCourse-hw2/CMakeLists.txt;13;add_test;/home/ivan/Desktop/CppDevCourse-hw2/CMakeLists.txt;0;")
