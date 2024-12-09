ee98c3971d1fad0ec5f967b26afcc8c7f32edd4c
IlyaKlyunev <ilaklunev@gmail.com>
2024-12-09 18:18:21 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
+#include <iostream> 
 
- int main () {
- std::cout << "Hello World!" << std::endl;
- }ssh-agent.sudo -s -Hexec ssh-agent bashexec ssh-agent zsh
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
