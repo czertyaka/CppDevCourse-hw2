564b998f23e1d29672dcb8328936a96cd16afa9a
A1r3t0 <113704274+A1r3t0@users.noreply.github.com>
2024-12-14 18:00:38 +0500

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artyomartyushin/IdeaProjects/elevatorEx/CppDevCourse-hw2
-# Build directory: /Users/artyomartyushin/IdeaProjects/elevatorEx/CppDevCourse-hw2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(main "/opt/homebrew/bin/python3" "-m" "test" "./main")
-set_tests_properties(main PROPERTIES  WORKING_DIRECTORY "/Users/artyomartyushin/IdeaProjects/elevatorEx/CppDevCourse-hw2/build" _BACKTRACE_TRIPLES "/Users/artyomartyushin/IdeaProjects/elevatorEx/CppDevCourse-hw2/CMakeLists.txt;13;add_test;/Users/artyomartyushin/IdeaProjects/elevatorEx/CppDevCourse-hw2/CMakeLists.txt;0;")
--- build/test.py
+++
@@ -1,7 +0,0 @@
-#!/bin/env python3
-
-import subprocess
-import sys
-
-proc = subprocess.run([sys.argv[1]], capture_output=True, shell=True, check=True)
-assert proc.stdout == b"Hello, World!\n"
